a1489320e18aabc16cab301e2a9658e4fb357839
juniya-1001 <juniya1001@icloud.com>
2025-04-05 19:04:28 +0900

    preview修正１

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -8,7 +8,6 @@ class CalendarsController < ApplicationController
 
   # 予定の保存
   def create
-    # binding.pry
     Plan.create(plan_params)
  # モデル名.create(カラム名: 保存する値)
     redirect_to action: :index
@@ -37,8 +36,7 @@ class CalendarsController < ApplicationController
       plans.each do |plan|
         today_plans.push(plan.plan) if plan.date == @todays_date + x
       end
-      days = { :month => (@todays_date + x).month, :date => (@todays_date+x).day, :plans => today_plans, :wday => wdays[(@todays_date  + x).wday]}
-      # wday => (@todays_date + x).wday }
+      days = { month: (@todays_date + x).month, date: (@todays_date+x).day, plans: today_plans, wday: wdays[(@todays_date  + x).wday]}
       @week_days.push(days)
     end
 
